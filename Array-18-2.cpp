7f6d03f329ad7c1c4430b23f001781bb2f1c22a1
Ayush35 <69398078+Ayush35@users.noreply.github.com>
2021-07-11 20:01:30 +0530

    Add files via upload

--- Array-18-2.cpp
+++ Array-18-2.cpp
@@ -1,20 +1,29 @@
 #include<iostream>
+#include<unordered_map>
 using namespace std;
 
 int findfun(int arr[],int n,int k)
 {
-    int count = 0;
-   for(int i=0;  i < n ; i++)
-   {
-       for (int j = i+1; j < n; j++)
-       {
-           if(( arr[i] + arr[j] ) == k)
-           {
-               count++;
-           }
-       }
-   }
-    return count;
+    int c = 0;
+    unordered_map<int, int> m;
+        
+    for(int i= 0 ; i<n ;i++)
+    {
+        int x = k - arr[i];
+            
+        if(m[x] == 0){
+            m[arr[i]]++;
+        }
+        else{
+                
+            c+= m[x];
+                
+            m[arr[i]]++;
+        }
+        m.at(arr[i]);
+        cout<<endl;
+    }
+    return c;
 }
 int main()
 {
@@ -23,4 +32,5 @@ int main()
     int p=findfun(arr,n,k);
     cout<<p;
     return 0;
+
 }
