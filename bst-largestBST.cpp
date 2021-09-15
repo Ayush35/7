85e9c285904d290599a210068b8f2de449e2d8bc
Ayush35 <69398078+Ayush35@users.noreply.github.com>
2021-09-15 21:58:10 +0530

    Add files via upload

---
+++ bst-largestBST.cpp
@@ -0,0 +1,57 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node*left;
+    struct Node* right;
+    Node(int val){
+        data = val; 
+        left = NULL;
+        right = NULL;
+    }
+};
+
+struct info{
+    int size; 
+    int max; 
+    int min;
+    int ans;
+    bool isBST;
+};
+
+info largestBSTinBT(Node* root){
+    if(root == NULL){
+        return {0,INT_MIN,INT_MAX,0,true};
+    }
+    if(root->left == NULL && root->right == NULL){
+        return {1, root->data , root->data ,1 , true};
+    }
+
+    info leftinfo = largestBSTinBT(root->left);
+    info rightinfo = largestBSTinBT(root->right);
+
+    info curr;
+    
+    curr.size = (1+ leftinfo.size + rightinfo.size);
+    if(leftinfo.isBST && rightinfo.isBST && leftinfo.max < root->data && rightinfo.min > root->data){
+            curr.min = min ( leftinfo.min , min(rightinfo.min , root->data));
+            curr.max = min(rightinfo.max , max(leftinfo.max , root->data));
+            curr.ans = curr.size;
+            curr.isBST = true;
+            return curr;
+    }
+    curr.ans = max(leftinfo.max , max(rightinfo.max , root->data));
+    curr.isBST = false;
+
+    return curr;
+}
+
+int main(){
+    struct Node* root = new Node(15);
+    root->left = new Node(20);
+    root->right = new Node(30);
+    root->left->left = new Node(5);
+    cout<<largestBSTinBT(root).ans;
+    return 0;   
+}
